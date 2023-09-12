ab245690960c18dd12e73a7e940b40b31b127254
bss-andoritonga <yohannes.ritonga@banksampoerna.com>
2023-09-12 11:59:02 +0700

    first initt

--- app/Views/test.php
+++ app/Views/test.php
@@ -4,6 +4,6 @@
     <title>CodeIgniter GCS Session Test</title>
 </head>
 <body>
-    <h1>Welcome, <?php echo $username; ?></h1>
+    <h1>Welcome, to the jungle <?php echo $username; ?></h1>
 </body>
 </html>
